565952688c1a97989fac6cf428da2b3c94e3d7c6
jin2fantasy <wb3803@unist.ac.kr>
2016-03-11 12:59:47 +0900

    wrong calculations corrected
    such as parenthesises, and signs are corrected to make the calculation
    correct

--- SineWaveguide/sweep.jl
+++ SineWaveguide/sweep.jl
@@ -1,8 +1,25 @@
 addprocs(8)
 using PyPlot
-require("D:\\JuliaScripts\\SineWaveguide\\dispersion.jl")
+require("D:\\JuliaScripts\\SineWaveguide\\test.jl")
 N = 2001
 f = linspace(190e9, 270e9, N)
-@everywhere detMNPQ1(x) = detMNPQ(x, 30/180*pi/p)
-val = pmap(detMNPQ1, f)
-plot(f, val)
+@everywhere β0 = linspace(0, pi/p, 19)
+@everywhere i = 1
+@everywhere detMNPQ1 = [x->detMNPQ(x, β0[j]) for j in 1:19]
+alldata = Array(Float64, N, 0)
+while i < 20
+    val = pmap(detMNPQ1[i], f)
+    alldata = [alldata val]
+    i += 1
+end
+plot(f, alldata, ".")
+plot(f, alldata[:,2])
+
+
+
+N=2001
+@everywhere detMNPQ2(x) = detMNPQ(x, big(100/180*pi/p))
+f = linspace(big(190e9), 270e9, N)
+f = linspace(209.1e9, 209.6e9, N)
+val2 = pmap(detMNPQ2, f)
+plot(f, float(convert(Array{BigFloat, 1}, val2), "."))
