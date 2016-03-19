1a0a16e8dc6341321ede2f3b6ddc174d7e5aa531
jin2fantasy <wb3803@unist.ac.kr>
2016-03-19 22:16:39 +0900

    pyplot -> gadfly

--- SineWaveguide/plot_dispersion.jl
+++ SineWaveguide/plot_dispersion.jl
@@ -1,17 +1,24 @@
 addprocs(8)
 using Plots
 using DataFrames
-@everywhere include("D:\\JuliaScripts\\SineWaveguide\\mysolver.jl")
-@everywhere include("D:\\JuliaScripts\\SineWaveguide\\test3.jl")
+@everywhere include("mysolver.jl")
+@everywhere include("test3.jl")
 @everywhere N = 361
 @everywhere β = linspace(0, 2pi/p, N)
 @everywhere detMNPQ1 = [x -> detMNPQ(x, β[i]) for i in 1:N]
 f = pmap(mysolver, detMNPQ1, repmat([280e9], N), repmat([380e9], N))
-pyplot(size = (600,400))
-plot(β*p/pi*180, 1e-9f, line=(:solid, 2, :black))
+gadfly(size = (400,300))
 simdis = readtable("WR03-mode1.txt", header=false, skipstart=2, separator=' ')
-plot!(0:10:360, simdis[2], line=:none, marker=:circle)
-show()
+plot(β*p/pi*180, 1e-9f, linewidth=4, label="Theory")
+scatter!(0:10:360, simdis[2], markersize=4, label="Simulation")
+title!("Dispersion curve")
+xaxis!("Phase shift")
+yaxis!("Frequency (GHZ)",
+    guidefont=Plots.Font("Helvetica",14,:hcenter,:vcetner,0.0,RGB{U8}(0.0,0.0,0.0)))
+xlims!(0, 360)
+xticks!(0:60:360,
+    tickfont=Plots.Font("Helvetica",11,:hcenter,:vcetner,0.0,RGB{U8}(0.0,0.0,0.0)))
+gui()
 
 for (index,item) in enumerate(f)
     if !(typeof(item) <: Number)
